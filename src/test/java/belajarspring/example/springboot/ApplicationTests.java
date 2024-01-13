6e77de33e68258b4e288364cb2a3bc0c52f6d52f
gregoriusdimas <gregorius.dimas@pactindo.com>
2024-01-13 13:58:19 +0700

    spring init

---
+++ src/test/java/belajarspring/example/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package belajarspring.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
